cf55ceb61b8a1ab5d967c4ad2f61c5aee5325bac
mashe4ka2411 <delongy88@gmail.com>
2024-09-30 18:30:23 +0300

    комит2

---
+++ app/keyboards.py
@@ -0,0 +1,5 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="add location"), KeyboardButton(text="view map")]
+])
