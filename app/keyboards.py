a17dd2629e90d0dee86b4bd70f658d1f19606906
Sekamov <ai.ege.178@gmail.com>
2024-10-01 18:37:14 +0300

    adding keyboard resize

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 #Reply клавиатура
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Получить геолокацию"),
-     KeyboardButton(text="Посмотреть карту")]
-])
+    [KeyboardButton(text="Получить геолокацию")],
+    [KeyboardButton(text="Посмотреть карту")],
+],resize_keyboard = True)
